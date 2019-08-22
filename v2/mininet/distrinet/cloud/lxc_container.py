13f1d7005f7fb21d3e96b228676bb5e108801241
Damien Saucez <damien.saucez@inria.fr>
2019-08-22 15:18:24 +0000

    tidy code

--- v2/mininet/distrinet/cloud/lxc_container.py
+++ v2/mininet/distrinet/cloud/lxc_container.py
@@ -490,22 +490,6 @@ class LxcNode (Node):
         self.ssh.waitTunneled()
 
     ## == mininet =============================================================
-    # File descriptor to node mapping support
-    # Class variables and methods
-
-    inToNode = {}  # mapping of input fds to nodes
-    outToNode = {}  # mapping of output fds to nodes
-    ## ========================================================================
-
-    # XXX - SAME
-    @classmethod
-    def fdToNode( cls, fd ):
-        """Return node corresponding to given file descriptor.
-           fd: file descriptor
-           returns: node"""
-        node = cls.outToNode.get( fd )
-        return node or cls.inToNode.get( fd )
-
 
     # XXX - OK
     # Command support via shell process in namespace
@@ -575,34 +559,6 @@ class LxcNode (Node):
             self.waitStarted()
             self.finalizeStartShell()
 
-    # XXX - SAME
-    def mountPrivateDirs( self ):
-        "mount private directories"
-        # Avoid expanding a string into a list of chars
-        assert not isinstance( self.privateDirs, BaseString )
-        for directory in self.privateDirs:
-            if isinstance( directory, tuple ):
-                # mount given private directory
-                privateDir = directory[ 1 ] % self.__dict__
-                mountPoint = directory[ 0 ]
-                self.cmd( 'mkdir -p %s' % privateDir )
-                self.cmd( 'mkdir -p %s' % mountPoint )
-                self.cmd( 'mount --bind %s %s' %
-                               ( privateDir, mountPoint ) )
-            else:
-                # mount temporary filesystem on directory
-                self.cmd( 'mkdir -p %s' % directory )
-                self.cmd( 'mount -n -t tmpfs tmpfs %s' % directory )
-
-    # XXX - SAME
-    def unmountPrivateDirs( self ):
-        "mount private directories"
-        for directory in self.privateDirs:
-            if isinstance( directory, tuple ):
-                self.cmd( 'umount ', directory[ 0 ] )
-            else:
-                self.cmd( 'umount ', directory )
-
     def _popen( self, cmd, **params ):
         """Internal method: spawn and return a process
             cmd: command to run (list)
@@ -638,18 +594,6 @@ class LxcNode (Node):
             time.sleep(0.0001)
         return task.result()
 
-    ## XXX - SAME
-    def readline( self ):
-        """Buffered readline from node, potentially blocking.
-           returns: line (minus newline) or None"""
-        self.readbuf += self.read( 1024 )
-        if '\n' not in self.readbuf:
-            return None
-        pos = self.readbuf.find( '\n' )
-        line = self.readbuf[ 0: pos ]
-        self.readbuf = self.readbuf[ pos + 1: ]
-        return line
-
     # XXX - OK
     def write( self, data ):
         """Write data to node.
@@ -683,15 +627,6 @@ class LxcNode (Node):
 
         # cleanup variables
         self.cleanup()
-
-
-    # XXX - SAME
-    def stop( self, deleteIntfs=False ):
-        """Stop node.
-           deleteIntfs: delete interfaces? (False)"""
-        if deleteIntfs:
-            self.deleteIntfs()
-        self.terminate()
 
      # XXX - OK
      # TODO DSA - should find a wait to do it correctly
@@ -777,40 +712,6 @@ class LxcNode (Node):
             data = data.replace( chr( 127 ), '' )
         return data
 
-    ## XXX - SAME
-    def waitOutput( self, verbose=False, findPid=True ):
-        """Wait for a command to complete.
-           Completion is signaled by a sentinel character, ASCII(127)
-           appearing in the output stream.  Wait for the sentinel and return
-           the output, including trailing newline.
-           verbose: print output interactively"""
-#        log = info if verbose else debug
-        output = ''
-        while self.waiting:
-            data = self.monitor( findPid=findPid )
-            output += data
-#            log( data )
-        return output
-
-    ## XXX - SAME:
-    def cmd( self, *args, **kwargs ):
-        """Send a command, wait for output, and return it.
-           cmd: string"""
-        verbose = kwargs.get( 'verbose', False )
-#        log = info if verbose else debug
-#        log( '*** %s : %s\n' % ( self.name, args ) )
-        if self.shell:
-            self.sendCmd( *args, **kwargs )
-            return self.waitOutput( verbose )
-        else:
-            warn( '(%s exited - ignoring cmd%s)\n' % ( self, args ) )
-
-    # XXX - SAME
-    def cmdPrint( self, *args):
-        """Call cmd and printing its output
-           cmd: string"""
-        return self.cmd( *args, **{ 'verbose': True } )
-
     # XXX - OK
     def popen( self, *args, **kwargs ):
         """
@@ -833,269 +734,6 @@ class LxcNode (Node):
 
     ####################################################################
 
-    # Interface management, configuration, and routing
-
-    # BL notes: This might be a bit redundant or over-complicated.
-    # However, it does allow a bit of specialization, including
-    # changing the canonical interface names. It's also tricky since
-    # the real interfaces are created as veth pairs, so we can't
-    # make a single interface at a time.
-
-    # XXX - SAME
-    def newPort( self ):
-        "Return the next port number to allocate."
-        if len( self.ports ) > 0:
-            return max( self.ports.values() ) + 1
-        return self.portBase
-
-    # XXX - SAME
-    def addIntf( self, intf, port=None, moveIntfFn=moveIntf ):
-        """Add an interface.
-           intf: interface
-           port: port number (optional, typically OpenFlow port number)
-           moveIntfFn: function to move interface (optional)"""
-        if port is None:
-            port = self.newPort()
-        self.intfs[ port ] = intf
-        self.ports[ intf ] = port
-        self.nameToIntf[ intf.name ] = intf
-        debug( '\n' )
-        debug( 'added intf %s (%d) to node %s\n' % (
-                intf, port, self.name ) )
-        if self.inNamespace:
-            debug( 'moving', intf, 'into namespace for', self.name, '\n' )
-            moveIntfFn( intf.name, self  )
-
-    # XXX - SAME
-    def delIntf( self, intf ):
-        """Remove interface from Node's known interfaces
-           Note: to fully delete interface, call intf.delete() instead"""
-        port = self.ports.get( intf )
-        if port is not None:
-            del self.intfs[ port ]
-            del self.ports[ intf ]
-            del self.nameToIntf[ intf.name ]
-
-    # XXX - SAME
-    def defaultIntf( self ):
-        "Return interface for lowest port"
-        ports = self.intfs.keys()
-        if ports:
-            return self.intfs[ min( ports ) ]
-        else:
-            warn( '*** defaultIntf: warning:', self.name,
-                  'has no interfaces\n' )
-
-    # XXX - SAME
-    def intf( self, intf=None ):
-        """Return our interface object with given string name,
-           default intf if name is falsy (None, empty string, etc).
-           or the input intf arg.
-
-        Having this fcn return its arg for Intf objects makes it
-        easier to construct functions with flexible input args for
-        interfaces (those that accept both string names and Intf objects).
-        """
-        if not intf:
-            return self.defaultIntf()
-        elif isinstance( intf, BaseString):
-            return self.nameToIntf[ intf ]
-        else:
-            return intf
-
-    # XXX - SAME
-    def connectionsTo( self, node):
-        "Return [ intf1, intf2... ] for all intfs that connect self to node."
-        # We could optimize this if it is important
-        connections = []
-        for intf in self.intfList():
-            link = intf.link
-            if link:
-                node1, node2 = link.intf1.node, link.intf2.node
-                if node1 == self and node2 == node:
-                    connections += [ ( intf, link.intf2 ) ]
-                elif node1 == node and node2 == self:
-                    connections += [ ( intf, link.intf1 ) ]
-        return connections
-
-    # XXX - SAME
-    def deleteIntfs( self, checkName=True ):
-        """Delete all of our interfaces.
-           checkName: only delete interfaces that contain our name"""
-        # In theory the interfaces should go away after we shut down.
-        # However, this takes time, so we're better off removing them
-        # explicitly so that we won't get errors if we run before they
-        # have been removed by the kernel. Unfortunately this is very slow,
-        # at least with Linux kernels before 2.6.33
-        for intf in list( self.intfs.values() ):
-            # Protect against deleting hardware interfaces
-            if ( self.name in intf.name ) or ( not checkName ):
-                intf.delete()
-                info( '.' )
-
-    # Routing support
-
-    # XXX - SAME
-    def setARP( self, ip, mac ):
-        """Add an ARP entry.
-           ip: IP address as string
-           mac: MAC address as string"""
-        result = self.cmd( 'arp', '-s', ip, mac )
-        return result
-
-    # XXX - SAME
-    def setHostRoute( self, ip, intf ):
-        """Add route to host.
-           ip: IP address as dotted decimal
-           intf: string, interface name"""
-        return self.cmd( 'route add -host', ip, 'dev', intf )
-
-    # XXX - SAME
-    def setDefaultRoute( self, intf=None ):
-        """Set the default route to go through intf.
-           intf: Intf or {dev <intfname> via <gw-ip> ...}"""
-        # Note setParam won't call us if intf is none
-        if isinstance( intf, BaseString ) and ' ' in intf:
-            params = intf
-        else:
-            params = 'dev %s' % intf
-        # Do this in one line in case we're messing with the root namespace
-        self.cmd( 'ip route del default; ip route add default', params )
-
-    # Convenience and configuration methods
-
-    # XXX - SAME
-    def setMAC( self, mac, intf=None ):
-        """Set the MAC address for an interface.
-           intf: intf or intf name
-           mac: MAC address as string"""
-        return self.intf( intf ).setMAC( mac )
-
-    # XXX - SAME
-    def setIP( self, ip, prefixLen=8, intf=None, **kwargs ):
-        """Set the IP address for an interface.
-           intf: intf or intf name
-           ip: IP address as a string
-           prefixLen: prefix length, e.g. 8 for /8 or 16M addrs
-           kwargs: any additional arguments for intf.setIP"""
-        return self.intf( intf ).setIP( ip, prefixLen, **kwargs )
-
-    # XXX - SAME
-    def IP( self, intf=None ):
-        "Return IP address of a node or specific interface."
-        return self.intf( intf ).IP()
-
-    # XXX - SAME
-    def MAC( self, intf=None ):
-        "Return MAC address of a node or specific interface."
-        return self.intf( intf ).MAC()
-
-    # XXX - SAME
-    def intfIsUp( self, intf=None ):
-        "Check if an interface is up."
-        return self.intf( intf ).isUp()
-
-    # The reason why we configure things in this way is so
-    # That the parameters can be listed and documented in
-    # the config method.
-    # Dealing with subclasses and superclasses is slightly
-    # annoying, but at least the information is there!
-
-    # XXX - SAME
-    def setParam( self, results, method, **param ):
-        """Internal method: configure a *single* parameter
-           results: dict of results to update
-           method: config method name
-           param: arg=value (ignore if value=None)
-           value may also be list or dict"""
-        name, value = list( param.items() )[ 0 ]
-        if value is None:
-            return
-        f = getattr( self, method, None )
-        if not f:
-            return
-        if isinstance( value, list ):
-            result = f( *value )
-        elif isinstance( value, dict ):
-            result = f( **value )
-        else:
-            result = f( value )
-        results[ name ] = result
-        return result
-
-    # XXX - SAME
-    def config( self, mac=None, ip=None,
-                defaultRoute=None, lo='up', **_params ):
-        """Configure Node according to (optional) parameters:
-           mac: MAC address for default interface
-           ip: IP address for default interface
-           ifconfig: arbitrary interface configuration
-           Subclasses should override this method and call
-           the parent class's config(**params)"""
-        # If we were overriding this method, we would call
-        # the superclass config method here as follows:
-        # r = Parent.config( **_params )
-        r = {}
-        self.setParam( r, 'setMAC', mac=mac )
-        self.setParam( r, 'setIP', ip=ip )
-        self.setParam( r, 'setDefaultRoute', defaultRoute=defaultRoute )
-        # This should be examined
-        self.cmd( 'ifconfig lo ' + lo )
-        return r
-
-    # XXX - SAME
-    def configDefault( self, **moreParams ):
-        "Configure with default parameters"
-        self.params.update( moreParams )
-        self.config( **self.params )
-
-    # XXX - SAME
-    # This is here for backward compatibility
-    def linkTo( self, node, link=Link ):
-        """(Deprecated) Link to another node
-           replace with Link( node1, node2)"""
-        return link( self, node )
-
-    # Other methods
-
-    # XXX - SAME
-    def intfList( self ):
-        "List of our interfaces sorted by port number"
-        return [ self.intfs[ p ] for p in sorted( self.intfs.keys() ) ]
-
-    # XXX - SAME
-    def intfNames( self ):
-        "The names of our interfaces sorted by port number"
-        return [ str( i ) for i in self.intfList() ]
-
-
-    # XXX - SAME
-    def __repr__( self ):
-        "More informative string representation"
-        intfs = ( ','.join( [ '%s:%s' % ( i.name, i.IP() )
-                              for i in self.intfList() ] ) )
-        return '<%s %s: %s pid=%s> ' % (
-            self.__class__.__name__, self.name, intfs, self.pid )
-    
-    # XXX - SAME
-    def __str__( self ):
-        "Abbreviated string representation"
-        return self.name
-
-    # Automatic class setup support
-    # XXX - SAME
-    isSetup = False
-
-    # XXX - SAME
-    @classmethod
-    def checkSetup( cls ):
-        "Make sure our class and superclasses are set up"
-        while cls and not getattr( cls, 'isSetup', True ):
-            cls.setup()
-            cls.isSetup = True
-            # Make pylint happy
-            cls = getattr( type( cls ), '__base__', None )
-
     # XXX - OK
     # TODO DSA we should check on machines but it would slow down
     @classmethod
@@ -1107,7 +745,6 @@ class LxcNode (Node):
 
     # == New methods ==========================================================
     def finalizeStartShell(self):
-
         # XXX - TODO - DSA should be file numbers 
         self.outToNode[ self.stdout ] = self
         self.inToNode[ self.stdin ] = self 
