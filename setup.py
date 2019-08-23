586c3793a15c5539f5ea4b009e574eedcdf5bf2d
Giuseppe1992 <giuseppedilena92@gmail.com>
2019-08-23 13:54:37 +0200

    automatic creation for .distrine/conf.yml file

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from pathlib import Path
+from shutil import copy
+
+# create .distrinet/conf.yml configuration in the user home directory
+home_dir = Path.home()
+conf_dir = home_dir / ".distrinet"
+conf_dir.mkdir(0o777, parents=True, exist_ok=True)
+copy('conf/conf.yml', str(conf_dir))
+
+# get the required packages form requiremets.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+required = list(filter(lambda x: not x.startswith("#") and not x.startswith("git"), required))
+print(f"packages required from requirements.txt: {required}")
 
 setup(
     name='Distrinet',
@@ -8,14 +22,14 @@ setup(
     url='https://github.com/Giuseppe1992/Distrinet/tree/parallel-execution',
     dependency_links=['http://github.com/mininet/mininet/tarball/master#egg=mininet',
                       "https://github.com/Giuseppe1992/mapping_distrinet.git"],
-    install_requires=[
-    ],
+    install_requires=required,
     license='MIT',
     author='Giuseppe Di Lena',
     author_email='giuseppedilena92@gmail.com',
     description='Distrinet v.1.1',
     data_files= [(".distrinet",["conf/conf.yml"])],
     entry_points={
-        'console_scripts': ['dmn=src.dmn:main']}
-
+        'console_scripts': ['dmn=src.dmn:main']},
+    include_package_data = True,
+    zip_safe = True
 )
