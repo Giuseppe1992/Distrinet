1ed5431a9462021e4f0269ec452a0634446e76dc
Giuseppe1992 <giuseppedilena92@gmail.com>
2019-08-23 10:06:57 +0200

    add setup.py add configuration example

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Distrinet',
+    version='1.1',
+    packages=[''],
+    url='https://github.com/Giuseppe1992/Distrinet/tree/parallel-execution',
+    license='MIT',
+    author='Giuseppe Di Lena',
+    author_email='giuseppedilena92@gmail.com',
+    description='Distrinet v.1.1'
+)
