5a311d288f00a154094a901ef94fe0c1426c84cd
Giuseppe1992 <giuseppedilena92@gmail.com>
2019-08-23 11:01:54 +0200

    cleaning requirements, add simple test, starting directory refactoring

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Distrinet',
     version='1.1',
-    packages=[''],
+    python_requires='>=3.6',
+    packages=find_packages(exclude=["src"]),
     url='https://github.com/Giuseppe1992/Distrinet/tree/parallel-execution',
+    dependency_links=['http://github.com/mininet/mininet/tarball/master#egg=mininet',
+                      "https://github.com/Giuseppe1992/mapping_distrinet.git"],
+    install_requires=[
+    ],
     license='MIT',
     author='Giuseppe Di Lena',
     author_email='giuseppedilena92@gmail.com',
-    description='Distrinet v.1.1'
+    description='Distrinet v.1.1',
+    data_files= [("~/.distrinet",["conf/conf.yml"])]
+
 )
---
+++ tests/test_dummy.py
@@ -0,0 +1,9 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
