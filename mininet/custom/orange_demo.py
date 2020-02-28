15603b45efa9ed93b2908f65c9be6b0e0f06904d
Giuseppe1992 <giuseppedilena92@gmail.com>
2020-02-28 10:05:12 +0100

    tests

--- mininet/custom/orange_demo.py
+++ mininet/custom/orange_demo.py
@@ -24,6 +24,40 @@ PREBUILD = [default_images, toDemo]
 TESTS = {'hadoop':demo}
 """
 
+def add_nagios_host(mn_nagios,name,hostname,ip):
+
+    """define host {
+        use                 generic-host
+        name                sparta
+        host_name           sparta.naginet
+        address             10.128.0.21
+        max_check_attempts  3
+        contacts            nagiosadmin
+        check_period           24x7
+        check_command          check-host-alive
+        notification_interval  60
+        notification_period    24x7
+    }"""
+    path= "/usr/local/nagios/etc/objects/localhost.cfg"
+
+    mn_nagios.cmd("""bash -c 'echo "#########################################" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "define host {" >> """+path)
+    mn_nagios.cmd("""bash -c 'echo "    use                 generic-host" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    name                {}" >> {}'""".format(path,name))
+    mn_nagios.cmd("""bash -c 'echo "    host_name           {}" >> {}'""".format(path, hostname))
+    mn_nagios.cmd("""bash -c 'echo "    address             {}" >> {}'""".format(path, ip))
+    mn_nagios.cmd("""bash -c 'echo "    max_check_attempts  1" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    contacts            nagiosadmin" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    check_period           24x7" >> {}""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    check_command          check-host-alive" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    check_interval         0.1" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    notification_interval  60" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "    notification_period    24x7" >> {}'""".format(path))
+    mn_nagios.cmd("""bash -c 'echo "}" >> """+path)
+    mn_nagios.cmd("""bash -c 'echo "#########################################" >> {}'""".format(path))
+
+
+
 def demo_test(mn):
     topo = mn.topo
     user_1, user_2, downloader_1, f1, f2, fbackup, streaming, http, nagios = [ mn.nameToNode[host] for host in ['user1','user2','user3','fw-1','fw-2','fw-b', "stream","http", "nagios"]]
@@ -44,6 +78,14 @@ def demo_test(mn):
     cmd=f"bash -c 'iptables -t nat -A POSTROUTING -p tcp -m tcp --dport 8080 -j MASQUERADE'"
     output("fbackup", fbackup.cmd(cmd))
 
+    add_nagios_host(nagios,"firewall-1", "firewall-1.firewall",ip=f1.IP())
+    add_nagios_host(nagios,"firewall-2", "firewall-2.firewall",ip=f2.IP())
+    add_nagios_host(nagios,"firewall-b", "firewall-b.firewall",ip=fbackup.IP())
+    add_nagios_host(nagios,"streaming", "streaming.server",ip=streaming.IP())
+    add_nagios_host(nagios,"httpServer", "http.server",ip=http.IP())
+    nagios.cmd("service nagios restart")
+
+
     CLI(mn)
 
 
@@ -52,7 +94,7 @@ class DemoTopo( Topo ):
 
     def build( self):
 
-    # highest node is a web node
+    # highest node is a user
         h1 = self.addHost( 'user1' ) #u1
         h2 = self.addHost( 'user2' ) #u2
         h3 = self.addHost( 'user3' ) #d1
