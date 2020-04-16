f25cdc9a83dd6132b3ba638a74499a2934e48c93
Damien Saucez <damien.saucez@gmail.com>
2020-04-16 19:04:15 +0000

    support of bastion' specific username

--- mininet/mininet/ssh.py
+++ mininet/mininet/ssh.py
@@ -8,11 +8,85 @@ from mininet.dutil import _info
 
 class SSH(object):
     """
-    Class to connect to a remote node using SSH
+    Class to interact with a remote node using SSH.
+
+    The class support direct connection to the host or connection via a
+    bastion. When a bastion is used, an SSH connection is established to the
+    bastion and is used as a tunnel to connect ot the host.
+    The tunnel is setup as follows:
+        <random_port>:`host`:`host_port` `bastion_username`@`bastion`:`bastion_port`
+    
+    Attributes
+    ----------
+    loop : asyncio.AbstractEventLoop 
+        The async event loop to which associate the connection.
+    host : str
+        Hostname or IP address of the host to connect to.
+    port : int
+        The SSH port number to use to connect to the host.
+    username : str
+        Username to use to connect to the host. 
+    bastion : str 
+        Hostname or IP address of the bastion to use to connect to the host.
+    bastion_port : int 
+        The SSH port number to use to connect to the bastion.
+    bastion_username: str 
+        Username to use to connect to the bastion.
+    client_keys : list
+        List with the path to the authentications keys to use. Each entry
+        consists of a path to the authentication key, e.g.,
+        ['/home/example/.ssh/id_rsa_a', '/home/example/keys/key1'].
+    task : _asyncio.Task
+        Current task in execution.
+    tasks : list
+        Current tasks in execution (when multiple tasks have been ordered)
+    tunnel : asyncssh.listener.SSHForwardListener
+        Actual tunnel through the bastion
+    conn : asyncssh.connection.SSHClientConnection
+        Actual SSH connection with the host
+        hostname to connect to
+    connection_host : str
+        Hostname to actually use to connect to the host.
+    connection_port : int
+        Port number to actually use to connect to the host.
+
     """
     def __init__(self, loop, host, port=22, username=None, bastion=None,
-                       bastion_port=22, client_keys=None,
-                       **params):
+                       bastion_port=22, bastion_username=None,
+                       client_keys=None, **params):
+        """
+        Initialize an SSH connection object to connect to a host using SSHv2.
+
+        Parameters
+        ----------
+        loop : asyncio.AbstractEventLoop 
+            The async event loop to which associate the connection.
+        host : str
+            Hostname or IP address of the host to connect to.
+        port : int (optional, default=22)
+            The SSH port number to use to connect to the host.
+        username : str (optional, default=None)
+            Username to use to connect to the host. If None is provided, the
+            username of the process is used.
+        bastion : str (optional, default=None)
+            Hostname or IP address of the bastion to use to connect to the
+            host. If None is provided, no bastion is used to connect to the
+            host.
+        bastion_port : int (optional, default=22)
+            The SSH port number to use to connect to the bastion.
+        bastion_username: str (optional, default=None)
+            Username to use to connect to the bastion. If None is provided, the
+            username of the process is used.
+        client_keys : list (optional, default=None)
+            List with the path to the authentications keys to use. Each entry
+            consists of a path to the authentication key, e.g.,
+            ['/home/example/.ssh/id_rsa_a', '/home/example/keys/key1'].
+
+        Notes
+        -----
+        No SSH operation is performed at this stage, it is only used to preset
+        the object attributes.
+        """
         # the node runs
         self.run = True
 
@@ -28,15 +102,11 @@ class SSH(object):
         # ssh bastion information
         self.bastion = bastion
         self.bastion_port = bastion_port
+        self.bastion_username = bastion_username
 
         # current task in execution
         self.task = None
         self.tasks = []
-
-        # are we waiting for a task to be executed
-        self.waiting = False
-
-        self.readbuf = ''
 
         # SSH tunnel through the bastion
         self.tunnel = None
@@ -58,9 +128,10 @@ class SSH(object):
         if self.bastion is not None:
             self.loop.create_task(self._tunnel(bastion=self.bastion,
                                                bastion_port=self.bastion_port,
+                                               bastion_username=self.bastion_username,
                                                host=self.host, port=self.port))
    
-    async def _tunnel(self, bastion, host, bastion_port=22, port=22):
+    async def _tunnel(self, bastion, host, bastion_port=22, port=22, bastion_username=None):
         """
         Establishes SSH local port forwarding to `host:port` via the bastion
         node (`bastion`:`bastion_port`).
@@ -69,35 +140,33 @@ class SSH(object):
         The local port number is obtained by calling `get_port()` on the
         `tunnel` attribute.
 
-
-        Attributes
+        Parameters 
         ----------
         bastion : str
             name of the bastion (i.e., SSH relay) to use to connect to
             `host`:`port`. 
         host : str
             name of the host to connect to.
-        bastion_port : int
-            SSH port number to use to connect to the bastion (default is 22).
-        port : int
-            SSH port number to use (default is 22).            
+        bastion_port : int (optional, default=22)
+            SSH port number to use to connect to the bastion.
+        port : int (optional, default=22)
+            SSH port number to use to connect to the host.
         """
-
         connect = partial(asyncssh.connect, known_hosts=None, client_keys=self.client_keys)
-        if self.username is not None:
-            connect = partial(connect, username=self.username)
+        if bastion_username is not None:
+            connect = partial(connect, username=bastion_username)
 
         async with connect(host=bastion, port=bastion_port) as conn:
             self.tunnel = await conn.forward_local_port('', 0, host, port)
-            _info ("tunnel {}:{}:{} {}@{}:{} \n".format(self.tunnel.get_port(), host, port, self.username, bastion, bastion_port))
+            _info ("tunnel {}:{}:{} {}@{}:{} \n".format(self.tunnel.get_port(), host, port, bastion_username, bastion, bastion_port))
             while self.run:
                 await asyncio.sleep(1)
 
     def waitTunneled(self):
         """
-        Waits that the tunnel is established (if needed) and updates `_host`
-        and `_port` attributes to use it instead of having a direct connection
-        to the host
+        Waits that the tunnel with the bastion is established (if needed) and
+        updates `connection_host` and `connection_port` attributes to use it
+        instead of having a direct connection to the host
         """
         if self.bastion is not None:
             while self.tunnel is None:
