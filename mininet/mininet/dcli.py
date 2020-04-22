f43aac6a9af11976cc9787d8dc7b3a8647754866
Damien Saucez <damien.saucez@gmail.com>
2020-04-22 12:26:53 +0000

    fully commented

--- mininet/mininet/dcli.py
+++ mininet/mininet/dcli.py
@@ -1,8 +1,41 @@
+"""
+author: Damien Saucez (damien.saucez@gmail.com) 
+"""
 from mininet.cli import CLI
 from mininet.log import info, output, error
-
+import sys
 
 class DCLI( CLI ):
+    """
+    A simple command-line interface for Distrinet.
+
+    Notes
+    -----
+    Extends `mininet.cli.CLI` to take into account the peculiarities of
+    Distrinet.
+
+    The prompt is changed to 'distrinet> ' by default.
+    
+    See also
+    --------
+    `mininet.cli`
+    """
+
+    def __init__( self, mininet, stdin=sys.stdin, script=None, prompt='distrinet> '):
+        """
+        Start and run interactive or batch mode CLI
+        mininet : mininet.distrinet.Distrinet
+            Distrinet network object.
+        stdin : file object
+            Standard input for CLI.
+        script : str
+            Script to run in batch mode.
+        prompt : str (optional, default='distrinet> ')
+            CLI prompt.
+        """ 
+        self.setPrompt(prompt)
+        super(DCLI, self).__init__(mininet=mininet, stdin=stdin, script=script)
+
     def do_xterm( self, line, term='xterm' ):
         """Spawn xterm(s) for the given node(s).
            Usage: xterm node1 node2 ..."""
@@ -24,3 +57,14 @@ class DCLI( CLI ):
         while node.waiting:
             v = node.monitor(timeoutms=1)
             output( v )
+
+    def setPrompt( self, prompt):
+        """
+        Set the CLI prompt.
+
+        Parameters
+        ----------
+        prompt : str
+            The prompt to use in the CLI.
+        """
+        self.prompt = prompt
